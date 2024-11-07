41be04dfba563c56b34f636fed0eb2da635cd07e
Jaime Castañeda <jaimecastaneda133@gmail.com>
2024-11-07 11:39:38 -0500

    Paso 4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 def saludo():
-    return "Hola CD/CI prueba pipeline"
+    return "Hola CD/CI prueba pipeline1"
 
 if __name__ == "__main__":
     print(saludo())
--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from main import saludo
 
 class TestSaludo(unittest.TestCase):
     def test_saludo(self):
-        self.assertEqual(saludo(), "Hola CD/CI prueba pipeline")
+        self.assertEqual(saludo(), "Hola CD/CI prueba pipeline1")
 
 if __name__ == "__main__":
     unittest.main()
